fac98ee5087429826a19ee3bede6db3a985d1567
tylermmy13-droid <tylermmy13-droid@users.noreply.github.com>
2026-06-26 20:20:54 +0800

    fix: 撤销误提交的已删除文件

--- public/paper-sites/sis-motion/static/js/zoom.js
+++
@@ -1,8 +0,0 @@
-// Initialize medium zoom.
-// note: $ = jquery
-$(document).ready(function() {
-    medium_zoom = mediumZoom('[data-zoomable]', {
-        margin: 100,
-        background: '#ffffffee',
-    })
-});
